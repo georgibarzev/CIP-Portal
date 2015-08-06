d49c530f5fd6a9a467c82223defafccf8fe97490
John Le Drew <jp@antz29.com>
2015-08-06 15:29:40 +0100

    moved the js out of the header
    stopped us recompiling app.js with JSX transform
    
    removed superflous dependencies, moved into app.js

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,16 +1,18 @@
 /** @jsx React.DOM */
 var React = require('react');
+var moment = require('moment');
 
-// Not ideal to use createFactory, but don't know how to use JSX to solve this
-// Posted question at: https://gist.github.com/sebmarkbage/ae327f2eda03bf165261
+
 var DateBox = require('./components/DateBox.js');
 var NewsBox = require('./components/news.js').NewsBox;
 var TileBox = require('./components/tiles.js').TileBox;
+
 
 React.render(
   <DateBox />,
   document.getElementById('date_box')
 );
+
 
 React.render(
   <NewsBox url="news/" />,
