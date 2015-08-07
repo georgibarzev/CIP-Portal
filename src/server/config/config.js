7dad0844e1a6c657bd5ffc59ffe08f34de27039c
georgibarzev <a581541@MC0QNAWC.ww930.my-it-solutions.net>
2015-08-07 16:51:33 +0100

    password config

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -20,16 +20,17 @@ config.express = {}
 config.express.host = process.env.VCAP_APP_HOST || '0.0.0.0';
 config.express.port = process.env.VCAP_APP_PORT || 3000;
 
-config.passport = {
-            strategy : 'saml',
-            saml : {
-                path : '/login/callback',
-                entryPoint : 'https://iam.cip-dev.canopy-cloud.com/openam/SSORedirect/metaAlias/ActiveDirectory/idp',
-                logoutUrl : 'https://iam.cip-dev.canopy-cloud.com/openam/IDPSloRedirect/metaAlias/ActiveDirectory/idp',
-                issuer : 'passport-saml',
-                callbackUrl: 'https://portal.cip-dev.canopy-cloud.com/login/callback',
-                cert: fs.readFileSync('src/server/cert/forgerock.pem', 'utf-8')
-            }
-};
+config.passport = {}
+
+config.passport.strategy = 'saml';
+config.passport.saml = {};
+
+config.passport.saml.path = '/login/callback';
+config.passport.saml.entryPoint = 'https://iam.cip-dev.canopy-cloud.com/openam/SSORedirect/metaAlias/ActiveDirectory/idp';
+config.passport.saml.logoutUrl = 'https://iam.cip-dev.canopy-cloud.com/openam/IDPSloRedirect/metaAlias/ActiveDirectory/idp';
+config.passport.saml.issuer = 'passport-saml';
+config.passport.saml.callbackUrl = 'https://portal.cip-dev.canopy-cloud.com/login/callback';
+config.passport.saml.cert = fs.readFileSync('src/server/cert/forgerock.pem', 'utf-8');
+
 
 module.exports = config;
