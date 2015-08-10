d2a930343552a01c5d41c6e16186e0cca287b817
georgibarzev <a581541@MC0QNAWC.ww930.my-it-solutions.net>
2015-08-10 19:04:58 +0100

    moved passport initiate to server

--- src/server/lib/http/routers/defaultRouter.js
+++ src/server/lib/http/routers/defaultRouter.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var samlstrategy = require('../../utils/passport');
+var passport = require('passport');
+var session = require('express-session');
 
 module.exports = function(app, passport){
 	"use strict";
