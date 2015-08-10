fdf3ec1d7cc52518aa334f1fb20c462707b85e42
georgibarzev <a581541@MC0QNAWC.ww930.my-it-solutions.net>
2015-08-10 18:29:15 +0100

    moved passport initiate to server

--- src/server/lib/http/routers/defaultRouter.js
+++ src/server/lib/http/routers/defaultRouter.js
@@ -1,8 +1,12 @@
 var express = require('express');
+var samlstrategy = require('../../utils/passport');
+var passport = require('passport');
+var session = require('express-session');
 
 module.exports = function(app, passport){
 	"use strict";
 
+	var router = new express.Router();
 
 	require('../routes/news.js')(router,app);
 	require('../routes/tiles.js')(router,app);
