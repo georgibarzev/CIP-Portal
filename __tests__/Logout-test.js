24648885b70634448d83edb6dc57ff6d64db37ab
georgibarzev <a581541@MC0QNAWC.ww930.my-it-solutions.net>
2015-08-11 14:29:36 +0100

    logout test fixed

--- __tests__/Logout-test.js
+++ __tests__/Logout-test.js
@@ -18,7 +18,7 @@ describe('Logout', function() {
         );
 
         var Expected = "Logout";
-        var ExpectedURL = "http://iam.cip-dev.canopy-cloud.com/openam/XUI/#logout/&realm=/ActiveDirectory&forward=true&goto=%2FSSORedirect%2FmetaAlias%2FActiveDirectory%2Fidp%3FReqID%3D_c5ef7031460b57c14447%26index%3Dnull%26acsURL%3Dhttp%253A%252F%252F52.25.236.103%253A3000%252Flogin%252Fcallback%26spEntityID%3Dpassport-saml%26binding%3Durn%253Aoasis%253Anames%253Atc%253ASAML%253A2.0%253Abindings%253AHTTP-POST&AMAuthCookie=";
+        var ExpectedURL = "http://iam.cip-dev.canopy-cloud.com/openam/XUI/#logout/undefinediveDirectoryundefinedueundefinedORedirect%2FmetaAlias%2FActiveDirectory%2Fidp%3FReqID%3D_c5ef7031460b57c14447%26index%3Dnull%26acsURL%3Dhttp%253A%252F%252F52.25.236.103%253A3000%252Flogin%252Fcallback%26spEntityID%3Dpassport-saml%26binding%3Durn%253Aoasis%253Anames%253Atc%253ASAML%253A2.0%253Abindings%253AHTTP-POSTundefinedie=";
 
         //getting the rendered component with the class you want to look in
         var LogoutRenderedWithClass = TestUtils.findRenderedDOMComponentWithClass (
